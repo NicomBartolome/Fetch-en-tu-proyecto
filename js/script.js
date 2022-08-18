b0371dcb6d367fd9fb24a18fe8e7ff60bf217019
Nicolas Bartolome <nicom.bartolome@gmail.com>
2022-08-17 21:53:40 -0300

    Desafio guardado

--- js/script.js
+++ js/script.js
@@ -30,33 +30,40 @@ function AbonoPromedio (Abono4,precio4,Abono6,precio6,Abono8,precio8,Abono10,pre
             let CtesAbonos4 = document.createElement('h3');
             CtesAbonos4.innerHTML = "El abono mas vendido es el de 4 Mb";
             document.body.append(CtesAbonos4);
+            localStorage.setItem('AbonoMV',"El abono mas vendido es el de 4 Mb")
             break;
         case Abono6:
             let CtesAbonos6 = document.createElement('h3');
             CtesAbonos6.innerHTML = "El abono mas vendido es el de 6 Mb";
             document.body.append(CtesAbonos6);
+            localStorage.setItem('AbonoMV',"El abono mas vendido es el de 6 Mb")
             break;
         case Abono8:
             let CtesAbonos8 = document.createElement('h3');
             CtesAbonos8.innerHTML = "El abono mas vendido es el de 8 Mb";
             document.body.append(CtesAbonos8);
+            localStorage.setItem('AbonoMV',"El abono mas vendido es el de 8 Mb")
             break;
         case Abono10:
             let CtesAbonos10 = document.createElement('h3');
             CtesAbonos10.innerHTML = "El abono mas vendido es el de 10 Mb";
             document.body.append(CtesAbonos10);
+            localStorage.setItem('AbonoMV',"El abono mas vendido es el de 10 Mb")
             break;
         case Abono20:
             let CtesAbonos20 = document.createElement('h3');
             CtesAbonos20.innerHTML = "El abono mas vendido es el de 20 Mb";
             document.body.append(CtesAbonos20);
+            localStorage.setItem('AbonoMV',"El abono mas vendido es el de 20 Mb")
             break;
         default:
             let CtesAbonos = document.createElement('h3');
             CtesAbonos.innerHTML = "No se pudo determinar cual es el abono mas vendido";
             document.body.append(CtesAbonos);
+            localStorage.setItem('AbonoMV',"No se pudo determinar cual es el abono mas vendido")
             break;          
     }
+
 }
 
 function ejecutarPrograma(){
@@ -101,8 +108,43 @@ let Iprom = document.createElement('h3');
 Iprom.innerHTML = "El ingreso por cliente promedio es de $" + IngresoPromedio;
 document.body.append(Iprom);
 
+const resultados = {Clientes: TotalCteIngresado, Dinero: TotalFacturado, IngresoP: IngresoPromedio};
+
+localStorage.setItem('fechaUC', new Date())
+localStorage.setItem('resultados', JSON.stringify(resultados));
+
 }
 
+if(localStorage.getItem('fechaUC') !== null){
+    let date = localStorage.getItem('fechaUC');
+    let fecha = document.createElement('h3');
+    fecha.innerHTML = "La ultima vez que se uso el programa fue " + date;
+    document.body.append(fecha);
+
+    let resultados = JSON.parse(localStorage.getItem('resultados'))
+    
+    let AMV = localStorage.getItem('AbonoMV');
+    let AMVv = document.createElement('h3');
+    AMVv.innerHTML = AMV + " en el ultimo calculo";
+    document.body.append(AMVv); 
+
+    let TCI = resultados.Clientes
+    let TCii = document.createElement('h3');
+    TCii.innerHTML = "La ultima vez se ingresaron " + TCI + " clientes";
+    document.body.append(TCii);
+
+    let TF = resultados.Dinero
+    let TFff = document.createElement('h3');
+    TFff.innerHTML = "La ultima vez que se ingresaron " + TF + " clientes";
+    document.body.append(TFff);
+
+    let IP = resultados.IngresoP
+    let IPpp = document.createElement('h3');
+    IPpp.innerHTML = "El ultimo calculo fue de un ingreso promedio de $" + IP + " por cliente";
+    document.body.append(IPpp);
+
+    
+}
 
 let boton = document.getElementById('ejecutar');
 boton.addEventListener('click',ejecutarPrograma);
